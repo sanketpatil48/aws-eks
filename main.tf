fb2ca13e8ecc592c1eb9a1f88948e3279b4515bd
sanketpatil48 <sp518954@gmail.com>
2026-01-02 22:44:38 +0530

    Inital commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  
+  cluster_endpoint_public_access = true
+  
+  create_cloudwatch_log_group = false
+
+  
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+}
---
+++ varibles.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "my-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
